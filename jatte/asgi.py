5bc9716a3c0269deca89f92ab9a6ad45f6d4a32e
Aashishchoudhary <33976894+Aashishchoudhary@users.noreply.github.com>
2024-07-27 10:54:41 +0530

    Update asgi.py

--- jatte/asgi.py
+++ jatte/asgi.py
@@ -7,7 +7,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 import django
-#django.setup()
+django.setup()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jatte.settings')
 
 
